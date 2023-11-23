3c5a1be122ed89a258fedbec22e872452d0a0677
limaismael8901 <limaismael8901@gmail.com>
2023-11-23 18:36:13 -0300

    Adicionar arquivos de projeto.

---
+++ CriandoCRUD/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace CriandoCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.Salvar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listContatos = new System.Windows.Forms.ListView();
+            this.txtbuscar = new System.Windows.Forms.TextBox();
+            this.BuscarBut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefone";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(16, 30);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(16, 76);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(16, 127);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 5;
+            // 
+            // Salvar
+            // 
+            this.Salvar.Location = new System.Drawing.Point(57, 164);
+            this.Salvar.Name = "Salvar";
+            this.Salvar.Size = new System.Drawing.Size(49, 20);
+            this.Salvar.TabIndex = 6;
+            this.Salvar.Text = "Salvar";
+            this.Salvar.UseVisualStyleBackColor = true;
+            this.Salvar.Click += new System.EventHandler(this.Salvar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Buscar contato";
+            // 
+            // listContatos
+            // 
+            this.listContatos.HideSelection = false;
+            this.listContatos.Location = new System.Drawing.Point(145, 59);
+            this.listContatos.Name = "listContatos";
+            this.listContatos.Size = new System.Drawing.Size(252, 97);
+            this.listContatos.TabIndex = 8;
+            this.listContatos.UseCompatibleStateImageBehavior = false;
+            // 
+            // txtbuscar
+            // 
+            this.txtbuscar.Location = new System.Drawing.Point(145, 30);
+            this.txtbuscar.Name = "txtbuscar";
+            this.txtbuscar.Size = new System.Drawing.Size(182, 20);
+            this.txtbuscar.TabIndex = 9;
+            // 
+            // BuscarBut
+            // 
+            this.BuscarBut.Location = new System.Drawing.Point(334, 30);
+            this.BuscarBut.Name = "BuscarBut";
+            this.BuscarBut.Size = new System.Drawing.Size(63, 23);
+            this.BuscarBut.TabIndex = 10;
+            this.BuscarBut.Text = "Buscar";
+            this.BuscarBut.UseVisualStyleBackColor = true;
+            this.BuscarBut.Click += new System.EventHandler(this.Buscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 193);
+            this.Controls.Add(this.BuscarBut);
+            this.Controls.Add(this.txtbuscar);
+            this.Controls.Add(this.listContatos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Salvar);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Button Salvar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listContatos;
+        private System.Windows.Forms.TextBox txtbuscar;
+        private System.Windows.Forms.Button BuscarBut;
+    }
+}
+
---
+++ CriandoCRUD/Form1.cs
@@ -0,0 +1,104 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CriandoCRUD
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        string sql = "datasource=localhost;username=root;password=123456;database=db_agenda";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            listContatos.View = View.Details;
+            listContatos.LabelEdit = true;
+            listContatos.AllowColumnReorder = true;
+            listContatos.FullRowSelect = true;
+            listContatos.GridLines = true;
+
+            listContatos.Columns.Add("Id", 30, HorizontalAlignment.Left);
+            listContatos.Columns.Add("Nome", 150, HorizontalAlignment.Left);
+            listContatos.Columns.Add("E-mail", 150, HorizontalAlignment.Left);
+            listContatos.Columns.Add("Telefone", 150, HorizontalAlignment.Left);
+        }
+
+        private void Salvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new MySqlConnection(sql);
+                conn.Open();
+
+                string comandInsert = "INSERT INTO contato (nome, email, telefone) " +
+                    "VALUES " +
+                    "('" + txtNome.Text + "', '" + txtEmail.Text + "', '" + txtTelefone.Text + "') ";
+
+                MySqlCommand commandInsert = new MySqlCommand(comandInsert, conn);
+
+                commandInsert.ExecuteReader();
+
+                MessageBox.Show("De certo, contato Salvo!!!");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void Buscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new MySqlConnection(sql);
+                conn.Open();
+
+                var query = "'%"+txtbuscar.Text+"%'";
+                var comandSelect = "SELECT * FROM contato WHERE nome LIKE " + query +
+                    " OR email LIKE " + query;
+
+                MySqlCommand commandSelect = new MySqlCommand(comandSelect, conn);
+                MySqlDataReader reader = commandSelect.ExecuteReader();
+
+                listContatos.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        reader.GetString(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                    };
+
+                    var linhaListContato = new ListViewItem(row);
+                    listContatos.Items.Add(linhaListContato);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
