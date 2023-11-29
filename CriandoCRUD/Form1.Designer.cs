10fb470fb699e3f7ddbca85a2e217c80023f14a7
limaismael8901 <limaismael8901@gmail.com>
2023-11-29 07:34:20 -0300

    Commit com a atualização passada

--- CriandoCRUD/Form1.Designer.cs
+++ CriandoCRUD/Form1.Designer.cs
@@ -43,7 +43,7 @@
             this.txtbuscar = new System.Windows.Forms.TextBox();
             this.BuscarBut = new System.Windows.Forms.Button();
             this.ClearFormulary = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.DeleteItem = new System.Windows.Forms.Button();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -167,22 +167,22 @@
             this.ClearFormulary.UseVisualStyleBackColor = true;
             this.ClearFormulary.Click += new System.EventHandler(this.ClearFormularyAndMouse_Click);
             // 
-            // button1
+            // DeleteItem
             // 
-            this.button1.Location = new System.Drawing.Point(237, 162);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 12;
-            this.button1.Text = "Deletar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Delete_Click);
+            this.DeleteItem.Location = new System.Drawing.Point(237, 162);
+            this.DeleteItem.Name = "DeleteItem";
+            this.DeleteItem.Size = new System.Drawing.Size(75, 23);
+            this.DeleteItem.TabIndex = 12;
+            this.DeleteItem.Text = "Deletar";
+            this.DeleteItem.UseVisualStyleBackColor = true;
+            this.DeleteItem.Click += new System.EventHandler(this.Delete_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(409, 193);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DeleteItem);
             this.Controls.Add(this.ClearFormulary);
             this.Controls.Add(this.BuscarBut);
             this.Controls.Add(this.txtbuscar);
@@ -219,7 +219,7 @@
         private System.Windows.Forms.Button ClearFormulary;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button DeleteItem;
     }
 }
 
---
+++ CriandoCRUD/Validation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CriandoCRUD
+{
+    internal class Validation
+    {
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string telefone { get; set; }
+
+        public Validation(string Nome, string Email, string Telefone)
+        {
+            this.nome = Nome;
+            this.email = Email;
+            this.telefone = Telefone;
+        }
+        public static bool valid(string nome, string email, string telefone)
+        {
+            if (checkNumber(telefone) == true && checkFields(nome, email, telefone) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool validcamp(string nome, string email, string telefone)
+        {
+            if (checkFields(nome, email, telefone) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private static bool checkNumber(string telefone)
+            { 
+                string input = telefone;
+                if (Regex.IsMatch(input, @"^[0-9]+$"))
+                {
+                    return true;
+                }
+                else 
+                { 
+                    return false;
+                }
+            }
+        private static bool checkFields(string nome, string email, string telefone)
+        {
+            if (string.IsNullOrEmpty(nome) &&
+                string.IsNullOrEmpty(email) &&
+                string.IsNullOrEmpty(telefone))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
